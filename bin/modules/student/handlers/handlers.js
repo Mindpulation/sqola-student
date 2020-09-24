d5ab9bd0a7050e88de3db5908442ff3a34d95973
amardito <amardito37@gmail.com>
2020-09-24 20:03:34 +0700

    Update Student Handlers

--- bin/modules/student/handlers/handlers.js
+++ bin/modules/student/handlers/handlers.js
@@ -1,19 +1,28 @@
 const wrapper = require('../../../helpers/utils/wrapper');
 const commandHandler = require('../repositories/commands/command_handler');
+import { save } from "mongooo/lib/mongo/insert.js";
+import Mongo from "mongooo/lib/mongo/index.js";
 
-const SigninStudent = (req, res) => {
+const mongo1 = new Mongo();
+
+const obj1 = mongo1.setup("ENV.database", "DataSiswa", "User");
+
+const SigninStudent = async (req, res) => {
+    await Promise.all(save(obj1, {
+        Email : req.body.Email,
+        Password : req.body.Password
+    }))
+}
+
+const SignupStudent = async (req, res) => {
 
 }
 
-const SignupStudent = (req, res) => {
+const UpdateStudent = async (req, res) => {
 
 }
 
-const UpdateStudent = (req, res) => {
-
-}
-
-const DeleteStudent = (req, res) => {
+const DeleteStudent = async (req, res) => {
 
 }
 
