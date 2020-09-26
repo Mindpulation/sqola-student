d275cb5ed16503019b63b42e35e81b6053883982
amardito <amardito37@gmail.com>
2020-09-26 15:16:18 +0700

    env config
    env add database and collection

--- config/default.js
+++ config/default.js
@@ -4,5 +4,7 @@ module.exports = {
     pathStudentEndpoint: process.env.PATH_STUDENT_ENDPOINT,
     dnsSentryUrl: process.env.DNS_SENTRY_URL,
     ports: process.env.PORT,
-    mongoDbStudentUrl: process.env.MONGO_DB_REDEEM_URL,
+    mongoDbStudentUrl: process.env.MONGO_URL,
+    mongoDbStudent: process.env.MONGO_DB,
+    mongoDbStudentCol: process.env.MONGO_COL,
 }
