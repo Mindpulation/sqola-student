b65cea95e39eaca422a34628ffbd897e8d1c082d
Indra <me@indra.codes>
2020-11-28 20:00:20 +0000

    Added datadog integration

--- bin/app/student.js
+++ bin/app/student.js
@@ -1,17 +1,23 @@
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
 const studentHandlers =  require('../modules/student/handlers/handlers')
 const express = require('express');
 const app = express.Router();
 
 app.post("/signin",async (req, res) => {
+    dogstatsd.increment('page.views')
     await studentHandlers.SigninStudent(req, res)
 });
 app.post("/signup",async (req, res) => {
+    dogstatsd.increment('page.views')
     await studentHandlers.SignupStudent(req, res)
 });
 app.post("/find",async (req, res) => {
+    dogstatsd.increment('page.views')
     await studentHandlers.FindStudent(req, res)
 });
 app.put("/update",async (req, res) => {
+    dogstatsd.increment('page.views')
     await studentHandlers.UpdateStudent(req, res)
 });
 app.delete("/:userId",async (req, res) => {
